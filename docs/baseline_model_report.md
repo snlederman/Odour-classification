1088decd98e585a94d40fca0f2e5cb543ae00e8a
BenSiv <bensiv92@gmail.com>
2023-02-12 08:17:46 +0200

    fixing IDs in cleaned data

---
+++ docs/baseline_model_report.md
@@ -0,0 +1,6 @@
+---
+title: The Locust antenna as an odor discriminator
+subtitle: Baseline model report
+geometry: "left=2cm, right=2cm, top=0cm, bottom=0cm"
+# pandoc -f markdown+hard_line_breaks --output resume.pdf resume.md
+---
--- src/prep/preprocessing.py
+++ src/prep/preprocessing.py
@@ -14,18 +14,22 @@ PROJECT_DIR = SCRIPT_PATH.split("src")[0]
 def main():
     """program skeleton"""
     data_file = os.path.join(PROJECT_DIR,"data", "raw", "single_odor.csv")
-    
+
     data = pd.read_csv(data_file)
 
     # removing unnecessary columns
     data.drop(columns=["date", "channel"], inplace=True)
 
-    labels = data["label"]
-    features = data.drop(columns=["label"])
+    labels = data[["label","ID"]].set_index("ID")
+    features = data.drop(columns=["label"]).set_index("ID")
 
     # scaling features
     scaler = StandardScaler()
-    features_scaled = pd.DataFrame(scaler.fit_transform(features))
+    features_scaled = pd.DataFrame(scaler.fit_transform(features), columns=features.columns, index=features.index)
+
+    # reset index for saving
+    labels.reset_index(inplace=True)
+    features_scaled.reset_index(inplace=True)
 
     # saving data
     labels.to_csv(os.path.join(PROJECT_DIR,"data", "cleaned", "labels.csv"), index=False)
