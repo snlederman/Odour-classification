9b41f4aef4e302117e437261a5daab5c363d9264
BenSiv <bensiv92@gmail.com>
2023-03-02 09:07:05 +0200

    cleaned preprocessing pipeline

--- src/pipeline.sh
+++ src/pipeline.sh
@@ -3,13 +3,13 @@
 # this is a pipline running the different scripts in the project
 
 # clean row data
-python src/prep/clean.py
+python prep/clean.py
 
 # scale cleaned data
-python src/prep/scale.py
+python prep/scale.py
 
 # augment cleaned data
-python src/prep/augment.py --number 100
+python prep/augment.py --number 100
 
 # augment scaled data
-python src/prep/augment.py --number 100 --scale
+python prep/augment.py --number 100 --scale
