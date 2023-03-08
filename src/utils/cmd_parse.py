aef8ca8feeb0fb92a4ddaaf31490b7d022dc356c
Ella Jewison <jewison.ella@gmail.com>
2023-03-08 16:34:15 +0100

    added the fourier flag to the cmd_parse file

--- src/utils/cmd_parse.py
+++ src/utils/cmd_parse.py
@@ -13,4 +13,5 @@ def get_args(argv=None):
     parser.add_argument("-s","--scale", action="store_true", help="standard scaling")
     parser.add_argument("-a","--augment", action="store_true", help="augmented data")
     parser.add_argument("-n","--number", type=int, help="repeats the action n times")
+    parser.add_argument("-f","--fourier", action="store_true", help="fourier transform data")
     return vars(parser.parse_args(argv))
